f1d59805b43ff0e9936d80c164afa5bedfe02110
Tom-atoes <69087922+Tom-atoes@users.noreply.github.com>
2021-12-07 14:20:14 +0000

    Create mobileapp.js

---
+++ folder1/mobileapp.js
@@ -0,0 +1,15 @@
+// Thomas S. 2021
+// DOCUMENTATION: doesnt exist apparantly, awesome!!! i lovvvvveeeeeeeee appshed its so AWESOME! :glee: :slight_smile: :heart_eyes:
+
+/* 
+i hate app shed - doesnt even support github :slight_frown:
+
+this is my apps JS code, this code is located under the "Settings" tab and then under the "Advanaced" tab in the Appshed Editor.
+
+i have made a github repo because appshed is awful to navigate and i dont want to drive myself mad.
+*/
+
+console.log("Hello World!") // there isnt even an output/console what the hell
+
+// for some god forsaken reason you cannot change the footer/tab-bar's background colour in the style page, it is the only element that has to be changed with CSS or JS.
+app.setTabBarBackgroundColor("#474747"); // no clue if this supports rgb so hex it is
